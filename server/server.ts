6f4327ccaa8ab1f4d15694488e6c24fb7971aa59
Himanshu Verma <himanshuverma4765@gmail.com>
2026-07-19 09:14:06 +0530

    basic error handelling middleware + register & login route setup

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,35 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+
+app.use("/api/auth", authRouter);
+
+
+// Error Handelling
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  res.status(500).json({ message: error.message });
 });
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
