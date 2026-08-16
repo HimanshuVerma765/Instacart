52e0cfac58a7e07b338813f7c8a0456bce6484bc
Himanshu Verma <himanshuverma4765@gmail.com>
2026-08-16 17:40:08 +0530

    ...

--- server/server.ts
+++ server/server.ts
@@ -35,6 +35,4 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: error.message });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+export default app;
