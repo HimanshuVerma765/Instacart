67a49c2b7b116122dfeeedc5c4eba2a17899e714
Himanshu Verma <himanshuverma4765@gmail.com>
2026-08-19 00:49:46 +0530

    implement getAdminStats endpoint for dashboard data retrieval

---
+++ server/controllers/adminController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/prisma.js";
+
+// Get admin dashboard data
+export const getAdminStats = async (req: Request, res: Response) => {
+  const [
+    totalOrders,
+    totalUsers,
+    totalProducts,
+    outOfStock,
+    totalPartners,
+    recentOrders,
+  ] = await Promise.all([
+    prisma.order.count({
+      where: { NOT: [{ paymentMethod: "card", isPaid: false }] },
+    }),
+
+    prisma.user.count(),
+    prisma.product.count(),
+    prisma.product.count({ where: { stock: 0 } }),
+    prisma.deliveryPartner.count(),
+    prisma.order.findMany({
+      where: { NOT: [{ paymentMethod: "card", isPaid: false }] },
+      orderBy: { createdAt: "desc" },
+      take: 8,
+      include: {
+        user: { select: { name: true, email: true } },
+        deliveryPartne: { select: { name: true, phone: true } },
+      },
+    }),
+  ]);
+
+  res.json({
+    totalOrders,
+    totalUsers,
+    totalProducts,
+    outOfStock,
+    totalPartners,
+    recentOrders,
+  });
+};
