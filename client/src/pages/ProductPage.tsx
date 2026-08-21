99abf09f0cee5f4820a87a159167cb99f87532c6
Himanshu Verma <himanshuverma4765@gmail.com>
2026-08-22 01:28:04 +0530

    feat: implement geolocation for address submission and update user addresses

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -2,7 +2,6 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import { useEffect, useState } from "react";
 import type { Product } from "../types";
-import { dummyProducts } from "../assets/assets";
 import Loading from "../components/Loading";
 import {
   ArrowLeftIcon,
